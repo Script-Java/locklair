a1c6009e93669561e26ba87416ea0f2910d5d115
Atrin Shahroudi <68357178+Script-Java@users.noreply.github.com>
2023-04-24 22:25:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from pass_utils import generate_pass, check_pass
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
@@ -23,12 +24,38 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
-app.route('/delete/<int:word_id>')
+@app.route('/delete/<int:word_id>')
 def delete(word_id):
     word = Word.query.filter_by(id = word_id).first()
     db.session.delete(word)
     db.session.commit()
     return redirect(url_for("index"))
+
+
+class Leaderboard(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(60))
+    password = db.Column(db.String(100))
+    score = db.Column(db.Integer())
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    words = Word.query.all()
+    pass_length = request.form.get('typeNumber')
+    name = request.form.get("name")
+    password = generate_pass(words, pass_length)
+    score = check_pass(password)
+    new_learderboard = Leaderboard(name=name,password=password,score=score)
+    db.session.add(new_learderboard)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/leaderboard')
+def leaderboard():
+    leader = Leaderboard.query.all()
+    return render_template("leaderboard.html", leader=leader)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

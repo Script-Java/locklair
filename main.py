f9d07741f25c81f2a63c6aa48f83f05d91c3f081
Script-Java <atrindev@gmail.com>
2025-01-10 19:27:16 -0600

    added

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+# Define the Word model
 class Word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(100))
-    
+    word = db.Column(db.String(100), nullable=False)
+
+# Define the Leaderboard model
+class Leaderboard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
 @app.route('/')
 def index():
     word_list = Word.query.all()
@@ -19,41 +27,37 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     word = request.form.get('word')
-    new_word = Word(word=word)
-    db.session.add(new_word)
-    db.session.commit()
+    if word:
+        new_word = Word(word=word)
+        db.session.add(new_word)
+        db.session.commit()
     return redirect(url_for("index"))
 
 @app.route('/delete/<int:word_id>')
 def delete(word_id):
-    word = Word.query.filter_by(id = word_id).first()
+    word = Word.query.get_or_404(word_id)
     db.session.delete(word)
     db.session.commit()
     return redirect(url_for("index"))
-
-
-class Leaderboard(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(60))
-    password = db.Column(db.String(100))
-    score = db.Column(db.Integer())
-
 
 @app.route('/generate', methods=['POST'])
 def generate():
     words = Word.query.all()
     pass_length = request.form.get('typeNumber')
     name = request.form.get("name")
+    if not (name and pass_length.isdigit()):
+        return redirect(url_for("index"))
+    pass_length = int(pass_length)
     password = generate_pass(words, pass_length)
     score = check_pass(password)
-    new_learderboard = Leaderboard(name=name,password=password,score=score)
-    db.session.add(new_learderboard)
+    new_leaderboard = Leaderboard(name=name, password=password, score=score)
+    db.session.add(new_leaderboard)
     db.session.commit()
-    return redirect(url_for("index"))
+    return redirect(url_for("leaderboard"))
 
 @app.route('/leaderboard')
 def leaderboard():
-    leader = Leaderboard.query.order_by(Leaderboard.score.desc())
+    leader = Leaderboard.query.order_by(Leaderboard.score.desc()).all()
     return render_template("leaderboard.html", leader=leader)
 
 @app.route('/whatsthis')
@@ -61,4 +65,6 @@ def whatsthis():
     return render_template("whatsthis.html")
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
